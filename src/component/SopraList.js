8724bebb6c1565ef55b4ba5d59d1bec5c9ceebf2
Jorge <staminna@protonmail.com>
2020-01-13 14:05:31 +0000

    Input box and search filter working as expected.

--- src/component/SopraList.js
+++ src/component/SopraList.js
@@ -1,27 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchData } from '../actions/actions';
+// import { store } from '../store/store'
 
 class SopraList extends Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      filter: "",
+      data: []
+    };
+      // store.subscribe(() => {
+      // // When state will be updated(in our case, when items will be fetched), 
+      // // we will update local component state and force component to rerender 
+      // // with new data.
+
+      // this.setState({
+      //   items: store.getState().items
+      // });
+
   }
 
-  handleClick() {
+
+  componentDidMount() {
     this.props.dispatch(fetchData());
-  }
+  
+  };
+
+  handleChange = event => {
+    this.setState({ filter: event.target.value });
+    this.setState((state, props) => ({
+      data: this.props.myData.Brastlewark
+    }))
+  };
 
   render() {
+
+    const { filter, data } = this.state;
+    const lowercasedFilter = filter.toLowerCase();
+    const filteredData = data.filter(item => {
+      return Object.keys(item).some(key =>
+        item[key].toString().toLowerCase().includes(lowercasedFilter)
+      );
+    });
     return (
       <div>
-          <button type="button" onClick={this.handleClick}>Play game</button>
-        {this.props.isFetching && <div>Loading...</div>}
-        <ul>
-          {console.log(this.props.myData.Brastlewark)}
-          {/* {this.props.myData.Brastlewark[0].map(d => <li key={d.id}>{d.Brastlewark[0].name}</li>)} */}
-        </ul>
-      </div>
+        <input value={filter} onChange={this.handleChange} />
+        {filteredData.map(item => (
+          <div key={item.id}>
+            <div> {item.name} </div>
+          </div>
+        ))}
+      {this.props.isFetching && <div>Loading...</div>}
+      <ul>
+        {console.log(this.props.myData.Brastlewark)}
+      </ul>
+    </div>
     );
   }
 }
