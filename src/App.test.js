9b887779a78621c1a9c48940b28e485ad57bd0ad
Jorge <staminna@protonmail.com>
2020-01-19 21:16:42 +0000

    Final commit, cleaning package.json

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
-import Input from './component/input';
-import fetchmock from 'jest-fetch-mock';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-Enzyme.configure({ adapter: new Adapter() })
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
